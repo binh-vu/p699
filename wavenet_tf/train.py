ace0263ca3d6a09e7b13a02e0f2a6c574a1f650f
Binh Vu <binh@toan2.com>
2019-12-10 19:02:51 -0800

    wip

--- wavenet_tf/train.py
+++ wavenet_tf/train.py
@@ -27,7 +27,7 @@ from wavenet_tf import WaveNetModel, optimizer_factory
 
 
 ROOT_DIR = Path(os.path.abspath(__file__)).parent.parent
-
+STARTED_DATESTRING = "{0:%Y-%m-%dT%H-%M-%S}".format(datetime.now())
 
 @dataclass
 class TrainParams:
@@ -44,19 +44,6 @@ class TrainParams:
     epsilon: float = 0.001
     momentum: float = 0.9
     l2_regularization_strength: float = 0.0
-    histograms: bool = False
-
-BATCH_SIZE = 1
-DATA_DIRECTORY = str(ROOT_DIR / 'data' / 'fma_small_25_16000')
-WAVENET_PARAMS = str(ROOT_DIR / 'data' / 'tf_wavenet_params.json')
-STARTED_DATESTRING = "{0:%Y-%m-%dT%H-%M-%S}".format(datetime.now())
-SAMPLE_SIZE = 100000
-L2_REGULARIZATION_STRENGTH = 0
-SILENCE_THRESHOLD = 0.3
-EPSILON = 0.001
-MOMENTUM = 0.9
-MAX_TO_KEEP = 5
-METADATA = False
 
 
 def get_dataloader(args):
@@ -75,31 +62,10 @@ def get_dataloader(args):
     return gen, approx_examples
 
 
-def train(args: TrainParams, model_params):
-    try:
-        directories = validate_directories(args)
-    except ValueError as e:
-        print("Some arguments are wrong:")
-        print(str(e))
-        return
-
-    logdir = directories['logdir']
-    restore_from = directories['restore_from']
-
-    # Even if we restored the model, we will treat it as new training
-    # if the trained model is written into an arbitrary location.
-    is_overwritten_training = logdir != restore_from
-
-    with open(args.wavenet_params, 'r') as f:
-        wavenet_params = json.load(f)
-
+def train(args: TrainParams, net, optimizer):
     # Load raw waveform from VCTK corpus.
     with tf.name_scope('create_inputs'):
         # Allow silence trimming to be skipped by specifying a threshold near
-        receptive_field = WaveNetModel.calculate_receptive_field(wavenet_params["filter_width"],
-                                                                 wavenet_params["dilations"],
-                                                                 wavenet_params["scalar_input"],
-                                                                 wavenet_params["initial_filter_width"])
         generator, approx_n_examples = get_dataloader(args)
         approx_epoch_size = approx_n_examples // args.batch_size
         dataset = tf.data.Dataset.from_generator(generator, tf.float32, (args.sample_size, 1))
@@ -107,22 +73,6 @@ def train(args: TrainParams, model_params):
 
         iterator = tf.compat.v1.data.make_initializable_iterator(dataset)
         audio_batch = iterator.get_next()
-
-    # Create network.
-    net = WaveNetModel(
-        batch_size=args.batch_size,
-        dilations=wavenet_params["dilations"],
-        filter_width=wavenet_params["filter_width"],
-        residual_channels=wavenet_params["residual_channels"],
-        dilation_channels=wavenet_params["dilation_channels"],
-        skip_channels=wavenet_params["skip_channels"],
-        quantization_channels=wavenet_params["quantization_channels"],
-        use_biases=wavenet_params["use_biases"],
-        scalar_input=wavenet_params["scalar_input"],
-        initial_filter_width=wavenet_params["initial_filter_width"],
-        histograms=args.histograms,
-        global_condition_channels=None,
-        global_condition_cardinality=None)
 
     if args.l2_regularization_strength == 0:
         args.l2_regularization_strength = None
@@ -136,7 +86,7 @@ def train(args: TrainParams, model_params):
     optim = optimizer.minimize(loss, var_list=trainable)
 
     # Set up logging for TensorBoard.
-    writer = tf.compat.v1.summary.FileWriter(logdir)
+    writer = tf.compat.v1.summary.FileWriter(args.log_dir)
     writer.add_graph(tf.compat.v1.get_default_graph())
     run_metadata = tf.compat.v1.RunMetadata()
     summaries = tf.compat.v1.summary.merge_all()
@@ -151,8 +101,8 @@ def train(args: TrainParams, model_params):
     saver = tf.compat.v1.train.Saver(var_list=tf.compat.v1.trainable_variables(), max_to_keep=args.max_checkpoints)
 
     try:
-        saved_global_step = load(saver, sess, restore_from)
-        if is_overwritten_training or saved_global_step is None:
+        saved_global_step = load(saver, sess, args.log_dir)
+        if saved_global_step is None:
             # The first training step will be saved_global_step + 1,
             # therefore we put -1 here for new or overwritten trainings.
             saved_global_step = -1
@@ -185,7 +135,7 @@ def train(args: TrainParams, model_params):
                 writer.add_run_metadata(run_metadata,
                                         'step_{:04d}'.format(step))
                 tl = timeline.Timeline(run_metadata.step_stats)
-                timeline_path = os.path.join(logdir, 'timeline.trace')
+                timeline_path = os.path.join(args.log_dir, 'timeline.trace')
                 with open(timeline_path, 'w') as f:
                     f.write(tl.generate_chrome_trace_format(show_memory=True))
             else:
@@ -196,7 +146,7 @@ def train(args: TrainParams, model_params):
             pbar.set_postfix(step=step, loss=loss_value, epoch=step // approx_epoch_size)
 
             if step > 0 and step % args.checkpoint_every == 0:
-                save(saver, sess, logdir, step)
+                save(saver, sess, args.log_dir, step)
                 last_saved_step = step
     except KeyboardInterrupt:
         # Introduce a line break after ^C is displayed so save message
@@ -204,7 +154,7 @@ def train(args: TrainParams, model_params):
         print()
     finally:
         if step > last_saved_step:
-            save(saver, sess, logdir, step)
+            save(saver, sess, args.log_dir, step)
 
 
 def save(saver, sess, logdir, step):
@@ -243,47 +193,6 @@ def load(saver, sess, logdir):
 def get_default_logdir(logdir_root):
     logdir = os.path.join(logdir_root, 'train', STARTED_DATESTRING)
     return logdir
-
-
-def validate_directories(args: TrainParams):
-    """Validate and arrange directory related arguments."""
-
-    # Validation
-    if args.log_dir and args.logdir_root:
-        raise ValueError("--logdir and --logdir_root cannot be "
-                         "specified at the same time.")
-
-    if args.log_dir and args.restore_from:
-        raise ValueError(
-            "--logdir and --restore_from cannot be specified at the same "
-            "time. This is to keep your previous model from unexpected "
-            "overwrites.\n"
-            "Use --logdir_root to specify the root of the directory which "
-            "will be automatically created with current date and time, or use "
-            "only --logdir to just continue the training from the last "
-            "checkpoint.")
-
-    # Arrangement
-    logdir_root = args.logdir_root
-    if logdir_root is None:
-        logdir_root = LOGDIR_ROOT
-
-    logdir = args.log_dir
-    if logdir is None:
-        logdir = get_default_logdir(logdir_root)
-        print('Using default logdir: {}'.format(logdir))
-
-    restore_from = args.restore_from
-    if restore_from is None:
-        # args.logdir and args.restore_from are exclusive,
-        # so it is guaranteed the logdir here is newly created.
-        restore_from = logdir
-
-    return {
-        'logdir': logdir,
-        'logdir_root': args.logdir_root,
-        'restore_from': restore_from
-    }
 
 
 if __name__ == '__main__':
